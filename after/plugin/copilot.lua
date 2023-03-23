56517e78ec531d9bed64dc708e56fca106033e67
Chris Peterson <Chris.Peterson444@gmail.com>
2023-03-23 08:20:07 -0700

    need to be able to cancel copilot. Also python issues

--- after/plugin/copilot.lua
+++ after/plugin/copilot.lua
@@ -1,1 +1,2 @@
 vim.keymap.set('i', '<C-\\>', 'copilot#Accept("")', {expr=true, silent=true, replace_keycodes = false})
+vim.keymap.set('i', '<C-e>', 'copilot#Dssmiss()', {expr=true, silent=true, replace_keycodes = false})
