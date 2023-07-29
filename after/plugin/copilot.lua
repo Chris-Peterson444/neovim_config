2de96b1d745276aa0cec9d00d9d2d861f0756c4c
Chris Peterson <chris.peterson444@gmail.com>
2023-07-28 20:37:17 -0700

    typo for <c-e> to dismiss copilot

--- after/plugin/copilot.lua
+++ after/plugin/copilot.lua
@@ -1,2 +1,2 @@
 vim.keymap.set('i', '<C-\\>', 'copilot#Accept("")', {expr=true, silent=true, replace_keycodes = false})
-vim.keymap.set('i', '<C-e>', 'copilot#Dssmiss()', {expr=true, silent=true, replace_keycodes = false})
+vim.keymap.set('i', '<C-e>', 'copilot#Dismiss()', {expr=true, silent=true, replace_keycodes = false})
