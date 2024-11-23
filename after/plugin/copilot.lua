5582c5652d9cd05955258c82f766d8e64a305767
Chris Peterson <chris.peterson@canonical.com>
2024-11-22 22:21:37 -0800

    Initial start with Kickstart

--- after/plugin/copilot.lua
+++
@@ -1,2 +0,0 @@
-vim.keymap.set('i', '<C-\\>', 'copilot#Accept("")', {expr=true, silent=true, replace_keycodes = false})
-vim.keymap.set('i', '<C-e>', 'copilot#Dismiss()', {expr=true, silent=true, replace_keycodes = false})
--- lua/vincen/let.lua
+++
@@ -1,3 +0,0 @@
-local config_path = os.getenv('HOME')
--- Specifically required for python based plugins
-vim.g.python3_host_prog = config_path .. '/.config/nvim/venv/bin/python3'
